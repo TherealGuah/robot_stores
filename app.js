522966decb460f5468972f210028b9e836aa34bd
pedrofn <pedrofechado@gmail.com>
2022-11-30 15:53:11 +0000

    added cors dependency

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const routes = require('./config/routes');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
+
 
 routes(app);
 
