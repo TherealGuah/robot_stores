ded89ae76aa8ac04544c4748cfe5b8d7b2d417c0
pedrofn <pedrofechado@gmail.com>
2022-11-30 09:59:44 +0000

    base app.js updated to connect routes

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// const routes = require('./config/routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// routes(app);
+
+app.listen(port, () => {
+    console.log(`Nodemon listening on port: ${port}`);
+});
