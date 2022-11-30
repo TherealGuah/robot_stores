73de313ceaf24863820537326d3cfc51466fb014
pedrofn <pedrofechado@gmail.com>
2022-11-30 11:47:35 +0000

    created addProduct(post) route

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,10 @@ const productsController = require('../controllers/productsController');
 
 const routes = (app) => {
     app.get('/products', productsController.getAllProducts);
+    //get single products
     app.get('/products/:id', productsController.getProduct);
+    //post single product
+    app.post('/products', productsController.addProduct);
 };
 
 module.exports = routes;
